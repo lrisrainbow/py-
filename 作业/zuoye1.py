cc55fb540c1f6b1ffc677ce46972e59221c4156d
lrisrainbow <3338367088@qq.com>
2025-03-09 23:41:29 +0800

    列表

---
+++ 作业/zuoye1.py
@@ -0,0 +1,79 @@
+# a1=eval(input("请输入一个自然数:"))
+# a2=bin(a1)
+# print("二进制:"+a2)
+# a4=oct(a1)
+# print("八进制:"+a4)
+# a3=hex(a1)
+# print("十六进制:"+a3)
+# a,b,c=list(map(float,input("请输入长方形的长,宽,高(数据请用空格分隔):").split()))
+#
+# #***********************
+# v=a*b*c
+# s=2*a*b+2*b*c+2*a*c
+#
+# #***********************
+#
+# print("长方体的体积=",v)
+#
+# print("长方体的表面积=",s)
+
+
+# s=input('输入一串字符: ')  #输入字符串：Hi,This is the first Python program
+# print(len(s))
+# print(max(s))
+# print(min(s))
+# print(ord('y'),ord(' '))
+# print(s.upper())
+# print(s.lower())
+# print(s.swapcase())
+# print(s.title())
+# print(s.find('Python'))
+# print(s.find('abc'))
+# print(s.index('Python'))
+#print(s.index('abc'))
+# import math
+# a1=eval(input('请输入半径'))
+# l=2*math.pi*a1
+# s=math.pi*a1*a1
+# c=round(l,2)
+# d=round(s,2)
+# e=round(a1,2)
+# print('圆的半径={:.2f}'.format(a1)+',周长={:.2f}'.format(c))
+# print('圆的半径={:.2f}'.format(a1)+',面积={:.2f}'.format(d))
+# import math
+# print('pi={:.0f}'.format(math.pi))
+# print('pi={:.2f}'.format(math.pi))
+# print('pi={:.4f}'.format(math.pi))
+# print(f'pi={math.pi:.0f}')
+# print(f'pi={math.pi:.2f}')
+# print(f'pi={math.pi:.4f}')
+# lst=[1,3,4,6]
+# for x in range(5):
+#     if x in lst:
+#         print(f'{x} in lst')
+#     else:
+#         print(f'{x} not in lst')
+# a=[1,3,4,6]
+# b=a
+# print(b is a)
+# # print(id(b),id(a))
+# b[0]=100
+# print(b[0],a[0])
+# c=a.copy()
+# print(c is a)
+# # print(id(c),id(a))
+# for i in range(0,20,5):
+#    row=range(i+1,i+6)
+#    print('\t'.join(map(str,row)))
+# def main( ):
+#
+#     if __name__ == '__main__':
+#
+#         print(1)
+#
+#     else:
+#
+#         print(2)
+#
+# main()
+print(int(3.69))
